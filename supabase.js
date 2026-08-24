1d08be222f925a7ce4e611e965d1f021bdd5a783
Hayiz <misho707599@gmail.com>
2026-08-25 00:55:05 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,3 +5,38 @@ const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
 );
+
+// إنشاء حساب جديد
+async function hayyizSignUp(email, password) {
+    return await supabaseClient.auth.signUp({
+        email,
+        password,
+        options: {
+            emailRedirectTo: 'https://just-c.github.io/adawati/'
+        }
+    });
+}
+
+// تسجيل الدخول
+async function hayyizSignIn(email, password) {
+    return await supabaseClient.auth.signInWithPassword({
+        email,
+        password
+    });
+}
+
+// تسجيل الخروج
+async function hayyizSignOut() {
+    return await supabaseClient.auth.signOut();
+}
+
+// الحصول على المستخدم الحالي
+async function hayyizGetUser() {
+    const { data, error } = await supabaseClient.auth.getUser();
+
+    if (error) {
+        return null;
+    }
+
+    return data.user;
+}
