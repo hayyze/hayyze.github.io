2bc8477f98b08368046219d348c82f811b1a2f33
Hayiz <misho707599@gmail.com>
2026-08-25 00:27:16 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = 'https://dzfdbjnqtgltuguqqdkz.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_zXE2LbHMIAiTFjrYTyb7GA_zrHGa1rv';
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
